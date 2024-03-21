05a8b3a34c14b6919af4cb4cb53e64d8899046f2
Andrii Malovanyi <malovaniy.an@gmail.com>
2024-03-21 23:13:23 +0200

    add views

--- train_station/views.py
+++ train_station/views.py
@@ -1,3 +1,74 @@
+from datetime import datetime
+
+from django.db.models import F, Count
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import extend_schema, OpenApiParameter
+from rest_framework import viewsets, mixins, status
+from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
 from django.shortcuts import render
 
-# Create your views here.
+from train_station.models import (
+    Crew,
+    TrainType,
+    Train,
+    Trip,
+    Ticket,
+    Station,
+    Route,
+    Order,
+)
+
+from train_station.serializer import (
+    TrainSerializer,
+    TicketSerializer,
+    TripSerializer,
+    TrainTypeSerializer,
+    RouteSerializer,
+    CrewSerializer,
+    OrderSerializer,
+    StationSerializer
+)
+
+
+class TrainTypeViewSet(viewsets.ModelViewSet):
+    queryset = TrainType.objects.all()
+    serializer_class = TrainTypeSerializer
+
+
+class TrainViewSet(viewsets.ModelViewSet):
+    queryset = Train.objects.all()
+    serializer_class = TrainSerializer
+
+
+class TripViewSet(viewsets.ModelViewSet):
+    queryset = Trip.objects.all()
+    serializer_class = TripSerializer
+
+
+class RouteViewSet(viewsets.ModelViewSet):
+    queryset = Route.objects.all()
+    serializer_class = RouteSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class StationViewSet(viewsets.ModelViewSet):
+    queryset = Station.objects.all()
+    serializer_class = StationSerializer
+
+
+class CrewViewSet(viewsets.ModelViewSet):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
